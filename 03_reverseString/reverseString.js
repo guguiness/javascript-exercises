32128ee7ee97cc1d51b076859351f95c62482573
guguiness <gus.nogueira@protonmail.com>
2022-11-01 14:55:01 -0300

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const words = str.split('');
+    words.reverse();
+    return words.join('');
 };
 
 // Do not edit below this line

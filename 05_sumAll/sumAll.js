dbd3a922d0e2435a64add1b287b1253d5e9a328b
guguiness <gus.nogueira@protonmail.com>
2022-11-03 19:44:30 -0300

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((num1 < 0 || typeof num1 !== 'number') || 
+        (num2 < 0 || typeof num2 !== 'number')) {
+            return 'ERROR';
+        } else {
+        let minor;
+        let major;
+        
+        if(num1 < num2) {
+            minor = num1;
+            major = num2;
+        } else {
+            minor = num2;
+            major = num1;
+        }
 
+        if(num1 === num2) {
+            return minor;
+        } else {
+            return major + sumAll(minor, major - 1);
+        }
+    }
 };
 
 // Do not edit below this line
